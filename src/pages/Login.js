f1c661cbcb5b3d92dd0ecb12e631a41b97bf2f69
hemanth <hemanthkumargrandhi45@gmail.com>
2026-07-23 15:30:55 +0530

    netlify changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useNavigate,Navigate } from 'react-router-dom'
+import { Link, useNavigate} from 'react-router-dom'
 import { toast } from 'react-toastify';
 import UseTitle from '../hooks/UseTitle';
 const Login = () => {
